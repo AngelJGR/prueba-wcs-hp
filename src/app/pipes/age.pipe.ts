fcb7d1ff729c8e3c3fbbf617ba5be44364c68d41
AngelJGR <ajguerreror@gmail.com>
2021-04-07 14:14:06 -0400

    Valida si birthday viene vacio

--- src/app/pipes/age.pipe.ts
+++ src/app/pipes/age.pipe.ts
@@ -6,7 +6,10 @@ import * as moment from 'moment';
 })
 export class AgePipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): number {
+  transform(value: string, ...args: unknown[]): number | string {
+    if (value === "") {
+      return 'Unknown'
+    }
     let birthday_arr:Array<string> = value.split("-")
     let birthDay: string = moment(new Date(parseInt(birthday_arr[2]), parseInt(birthday_arr[1]) - 1, parseInt(birthday_arr[0]))).format()
     let age:number = moment(Date.now()).diff(birthDay, 'years')

16258f7025c5fa100319990d0c5b4b97075b51ad
AngelJGR <ajguerreror@gmail.com>
2021-04-07 11:02:39 -0400

    Añade ruta para house

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HousesComponent } from './components/houses/houses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'house', component: HousesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

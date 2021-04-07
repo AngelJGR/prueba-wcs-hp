91593c0d6adb7b0ea263d565b92982b2b5e720e8
AngelJGR <ajguerreror@gmail.com>
2021-04-07 17:07:17 -0400

    Crea metodo para obtener los datos del staff y los muestra en la tabla

--- src/app/components/staff/staff.component.ts
+++ src/app/components/staff/staff.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HpService } from 'src/app/services/hp.service';
+
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-staff',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _hpService: HpService
+  ) { }
 
   ngOnInit(): void {
+    this.getStaff()
+  }
+
+  staff$
+  displayedColumns$:string[] = ['name', 'patronus', 'age', 'image'];
+  isLoadingResults:boolean = false
+
+  getStaff () {
+    this.staff$ = new MatTableDataSource<any>()
+    this.isLoadingResults = true
+    this._hpService.getStaff()
+      .subscribe(res => {
+        this.staff$.data = res
+        this.isLoadingResults = false
+      }, error => {
+        this.isLoadingResults = false
+        console.log(error)
+      })
   }
 
 }

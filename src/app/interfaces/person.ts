e5c4bee412327a4b51c3746287f61bc6cd3d3913
AngelJGR <ajguerreror@gmail.com>
2021-04-07 17:35:02 -0400

    Crea interfaces person y wand

---
+++ src/app/interfaces/person.ts
@@ -0,0 +1,20 @@
+import { Wand } from "./wand"
+
+export interface Person {
+  name: string
+  species: string
+  gender: string
+  house: string
+  dateOfBirth: string
+  yearOfBirth: number
+  ancestry: string
+  eyeColour: string
+  hairColour: string
+  wand: Wand
+  patronus: string
+  hogwartsStudent: boolean
+  hogwartsStaff: boolean
+  actor: string
+  alive: boolean
+  image: string
+}

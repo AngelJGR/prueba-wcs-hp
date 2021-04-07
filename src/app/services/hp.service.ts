d396274d2ead8319167bbcc4699f08050fb7c07c
AngelJGR <ajguerreror@gmail.com>
2021-04-07 17:06:12 -0400

    Crea métodos para obtener staff y estudiantes del api

--- src/app/services/hp.service.ts
+++ src/app/services/hp.service.ts
@@ -8,13 +8,21 @@ import { HttpClient, HttpParams } from '@angular/common/http'
 
 export class HpService {
 
-  api_url:string = 'http://hp-api.herokuapp.com/api/characters/house/'
+  api_url:string = 'http://hp-api.herokuapp.com/api/characters'
 
   constructor(
     private _http: HttpClient
   ) { }
 
   getHouse(house:string):Observable<any> {
-    return this._http.get<any>(`${this.api_url}${house}`)
+    return this._http.get<any>(`${this.api_url}/house/${house}`)
+  }
+
+  getStaff():Observable<any> {
+    return this._http.get<any>(`${this.api_url}/staff`)
+  }
+
+  getStudents():Observable<any> {
+    return this._http.get<any>(`${this.api_url}/students`)
   }
 }

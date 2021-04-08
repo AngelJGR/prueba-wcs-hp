b35715fb49e6d9bbe44c8eba01568a282b79d941
AngelJGR <ajguerreror@gmail.com>
2021-04-08 00:29:38 -0400

    Importa modules de material para botones, inputs, dialogs y icons

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -4,11 +4,19 @@ import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialComponents = [
   MatSelectModule,
   MatTableModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDialogModule,
+  MatInputModule
 ]
 
 @NgModule({

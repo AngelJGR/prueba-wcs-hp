749355b64cfac401da956c6efd5b76c12fea1cff
AngelJGR <ajguerreror@gmail.com>
2021-04-08 00:30:26 -0400

    Actualiza los campos no requeridos para esta aplicación

--- src/app/interfaces/person.ts
+++ src/app/interfaces/person.ts
@@ -2,19 +2,19 @@ import { Wand } from "./wand"
 
 export interface Person {
   name: string
-  species: string
-  gender: string
-  house: string
+  species?: string
+  gender?: string
+  house?: string
   dateOfBirth: string
-  yearOfBirth: number
-  ancestry: string
-  eyeColour: string
-  hairColour: string
-  wand: Wand
+  yearOfBirth?: number
+  ancestry?: string
+  eyeColour?: string
+  hairColour?: string
+  wand?: Wand
   patronus: string
-  hogwartsStudent: boolean
-  hogwartsStaff: boolean
-  actor: string
-  alive: boolean
+  hogwartsStudent?: boolean
+  hogwartsStaff?: boolean
+  actor?: string
+  alive?: boolean
   image: string
 }

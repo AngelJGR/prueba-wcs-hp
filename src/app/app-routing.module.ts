1e989f0076d9afebf371507e9a5ec220f7ab8dc0
AngelJGR <ajguerreror@gmail.com>
2021-04-09 13:55:55 -0400

    Crea ruta para home, redireccionamiento y link a home desde imagen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { HousesComponent } from './components/houses/houses.component';
 import { StaffComponent } from './components/staff/staff.component';
 import { StudentsComponent } from './components/students/students.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'house', component: HousesComponent },
   { path: 'staff', component: StaffComponent },
-  { path: 'students', component: StudentsComponent }
+  { path: 'students', component: StudentsComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
